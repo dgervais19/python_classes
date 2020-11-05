672fd2f02396a25d0e941d73138b9b6e112abccd
Donovan Gervais <applemac@Donovans-MacBook-Pro-6.local>
2020-11-05 10:25:20 +0000

    initialising a class

--- classes/dog_classes.py
+++ classes/dog_classes.py
@@ -8,18 +8,21 @@
 
 class Dog:
 
-    animal_kind = "canin"
+    def __init__(self, name, colour): # initialising Dog Class
+        self.name = name
+        self.colour = colour
+        self.animal_kind = "canin"
+
+
 
     def bark(self): # self refers to current class
         return "woof"
 # in order to execute a class we have to create an object of this class
-fido = Dog()  # creating an object of our Dog class also and instant of the class
-lassie = Dog()
+fido = Dog("fido", "brown")  # creating an object of our Dog class also and instant of the class
 
-print(fido) # This would print the path of where you can find whats in the Dog() class
-
+print(fido.name) # This would print the path of where you can find whats in the Dog() class
 print(fido.animal_kind) # prints the animal_kind variable in the fido/dog() class
-
-fido.animal_kind = "Big Dog" # this changes the animal_kind for fido but not for the whole Dog() class
-print(fido.animal_kind)
-print(lassie.animal_kind)  # This means that the animal_kind for lassie wont change. 
+print(fido.color)
+print(fido.bark())
+## fido.animal_kind = "Big Dog" # this changes the animal_kind for fido but not for the whole Dog() class
+## print(lassie.animal_kind)  # This means that the animal_kind for lassie wont change.
